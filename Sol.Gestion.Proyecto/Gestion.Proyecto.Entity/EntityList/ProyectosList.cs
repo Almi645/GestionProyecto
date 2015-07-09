575458cc603a7b9b8132baf3510b90da7b14ec51
Almi645 <miguel.ma.645@gmail.com>
2015-07-08 23:03:52 -0500

    d

---
+++ Sol.Gestion.Proyecto/Gestion.Proyecto.Entity/EntityList/ProyectosList.cs
@@ -0,0 +1,13 @@
+﻿using Gestion.Proyecto.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion.Proyecto.Entity.EntityList
+{
+    public class ProyectosList : List<Proyectos>
+    {
+    }
+}
---
+++ Sol.Gestion.Proyecto/Gestion.Proyecto.Entity/Others/Paginacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestion.Proyecto.Entity.Other
+{
+    public class Paginacion
+    {
+        public int Page { get; set; }
+
+        public int RowsPerPage { get; set; }
+
+        public int RowCount { get; set; }
+
+        public string SortType { get; set; }
+
+        public string SortDir { get; set; }
+
+        public string FooterDesc { get; set; }
+
+        public string Action { get; set; }
+    }
+}
