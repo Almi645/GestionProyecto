acebd9dc3467a52ec45bfcf57bc5a4a2a8f03594
Almi645 <miguel.ma.645@gmail.com>
2015-07-10 23:46:15 -0500

    ultima

---
+++ Sol.Gestion.Proyecto/Gestion.Proyecto.Web/Controllers/ProyectoDetalleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gestion.Proyecto.Web.Controllers
+{
+    public class ProyectoDetalleController : Controller
+    {
+        // GET: ProyectoDetalle
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+    }
+}
