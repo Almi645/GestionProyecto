8a3e4b51ff67c793ee6691c6d9de09905fb1ac6e
Almi645 <miguel.ma.645@gmail.com>
2015-07-13 17:55:31 -0500

    dd

--- Sol.Gestion.Proyecto/Gestion.Proyecto.Web/Controllers/ProyectoController.cs
+++ Sol.Gestion.Proyecto/Gestion.Proyecto.Web/Controllers/ProyectoController.cs
@@ -29,6 +29,7 @@ namespace Gestion.Proyecto.Web.Controllers
             model.Proyectos = new Proyectos();
             model.PersonaList = new ProyectosList();
             model.Paginacion = UtilGrid.MyPaginationDefault<Paginacion>(string.Empty, string.Empty, Constants.RowsPerPage.Int());
+            ModelState.Remove("Proyectos.Codigo");
             return View(model);
         }
 
